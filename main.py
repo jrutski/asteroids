f9d696520294353eb35e4dbea748d4d177981646
jacobrutski <jacob.rutski@gmail.com>
2025-06-10 10:20:55 -0400

    C2-L3 main.py tick

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
 
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
