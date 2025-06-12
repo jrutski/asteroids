530155dfca80a6cc069be0746f0e96bd30694efa
jacobrutski <jacob.rutski@gmail.com>
2025-06-12 11:41:37 -0400

    C2-L6 kbd event parse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -17,7 +19,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
         screen.fill("black", rect=None, special_flags=0)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
